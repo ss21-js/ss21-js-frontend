22ce13208dcbf0c21065f7911da1ae5a95d90b45
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-04-06 19:17:30 +0200

    Updates

--- src/ThemeWrapper.tsx
+++ src/ThemeWrapper.tsx
@@ -8,18 +8,16 @@ import * as React from 'react';
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			light: '#e5e5e5',
-			main: '#727272',
-			dark: '#363839',
-			contrastText: '#fff',
-		},
-		secondary: {
-			light: '#ff5e50',
-			main: '#e41e26',
-			dark: '#a90000',
+			light: '#5768D8',
+			main: '#475BD8',
+			dark: '#354AD4',
 			contrastText: '#fff',
 		},
 	},
+	shape: {
+		borderRadius: 6,
+	},
+	spacing: (factor) => `${0.25 * factor}rem`,
 });
 
 interface Props {
