72b936c2aacfda1952d8f427b19c268e5dfc8e9d
Carina Wehr <carina.wehr@stud.fh-rosenheim.de>
2021-04-13 18:38:58 +0200

    JS-42-styled components added

---
+++ src/styles/fonts.tsx
@@ -0,0 +1,16 @@
+import { css } from 'styled-components';
+
+export const fontSmall = css`
+	font-size: 13px;
+	line-height: 1.2;
+`;
+
+export const fontMedium = css`
+	font-size: 15px;
+	line-height: 1.5;
+`;
+
+export const fontLarge = css`
+	font-size: 18px;
+	line-height: 1.6;
+`;
