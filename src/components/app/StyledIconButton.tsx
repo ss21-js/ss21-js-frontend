b12dff02bda68005304d6ec2aade9b2b30d0e53c
Carina Wehr <carina.wehr@stud.fh-rosenheim.de>
2021-04-22 18:24:21 +0200

    JS-36-prettier

--- src/components/app/StyledIconButton.tsx
+++ src/components/app/StyledIconButton.tsx
@@ -3,16 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import IconButton from '@material-ui/core/IconButton';
 
 export interface IconButtonProps {
-    icon: IconProp;
-    onClick: () => void;
+	icon: IconProp;
+	onClick: () => void;
 }
 
-
-const StyledIconButton: React.FC<IconButtonProps > = ({ icon, onClick }) => (
-    <IconButton onClick={onClick}>
-            <FontAwesomeIcon icon={icon} />
-    </IconButton>
+const StyledIconButton: React.FC<IconButtonProps> = ({ icon, onClick }) => (
+	<IconButton onClick={onClick}>
+		<FontAwesomeIcon icon={icon} />
+	</IconButton>
 );
 
 export default StyledIconButton;
-
