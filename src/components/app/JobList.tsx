c2a46eb6fe4b133b4d4d95282847cf042990559b
Carina Wehr <carina.wehr@stud.fh-rosenheim.de>
2021-04-22 14:23:33 +0200

    JS-33-props and job model adjusted

--- src/components/app/JobList.tsx
+++ src/components/app/JobList.tsx
@@ -1,45 +1,41 @@
 import { css } from '@emotion/react';
 import { useTheme } from '@material-ui/core';
-import Checkbox from "@material-ui/core/Checkbox";
+import Checkbox from '@material-ui/core/Checkbox';
 import Chip from '@material-ui/core/Chip';
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import React from "react";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import React from 'react';
+import { Job } from 'src/model/job';
 
 interface JobListProps {
-    counter: number;
+	counter: number;
+	jobs: Job[];
 }
 
-interface Job {
-  name: string;
-}
+const JobList: React.FC<JobListProps> = ({ counter, jobs }) => {
+	const theme = useTheme();
 
-const jobs: Job[] = [{name: "Vollzeitjob"},{name: "Teilzeitjob"},{name: "Praktikumsstelle"},{name: "Ausbildungsstelle"},{name: "Remotejob"}]
-
-const JobList: React.FC<JobListProps > = ({counter}) => {
-  const theme = useTheme();
-
-  return (
-    <List component="div" disablePadding>
-        {jobs.map( job => (
-            <ListItem key={job.name} button 
-                css={css`
-                    padding-left: ${theme.spacing(4)};
-                `}
-            >
-                <ListItemIcon>
-                <Checkbox 
-                    color="primary"
-                />
-            </ListItemIcon>
-            <ListItemText primary={job.name} />
-                {/* TODO: counter-Function ergänzen, sobald es vom BE verfügbar ist */}
-                <Chip label={counter} />
-            </ListItem>
-        ))}
-    </List>
-  );
-}
+	return (
+		<List component="div" disablePadding>
+			{jobs.map((job) => (
+				<ListItem
+					key={job.index}
+					button
+					css={css`
+						padding-left: ${theme.spacing(4)};
+					`}
+				>
+					<ListItemIcon>
+						<Checkbox color="primary" />
+					</ListItemIcon>
+					<ListItemText primary={job.title} />
+					{/* TODO: counter-Function ergänzen, sobald es vom BE verfügbar ist */}
+					<Chip label={counter} />
+				</ListItem>
+			))}
+		</List>
+	);
+};
 export default JobList;
