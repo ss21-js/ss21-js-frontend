fe495d9667efcd611d926a0287dce9c83166b7da
Carina Wehr <carina.wehr@stud.fh-rosenheim.de>
2021-04-25 10:10:53 +0200

    JS-39-linter

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,29 +1,24 @@
-import Tab from "@material-ui/core/Tab";
-import Tabs from "@material-ui/core/Tabs";
-import React from "react";
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import React from 'react';
 
 export interface TabBarProps {
 	tabs: string[];
 }
 
 const TabBar: React.FC<TabBarProps> = ({ tabs }) => {
-  	const [value, setValue] = React.useState(0);
+	const [value, setValue] = React.useState(0);
 
 	const handleChange = (_event: React.ChangeEvent<{}>, newValue: number) => {
 		setValue(newValue);
 	};
 
 	return (
-		<Tabs
-        value={value}
-        onChange={handleChange}
-        variant="fullWidth"
-        textColor="primary"
-      >
+		<Tabs value={value} onChange={handleChange} variant="fullWidth" textColor="primary">
 			{tabs.map((tab) => (
-                <Tab label={tab}/>
+				<Tab label={tab} />
 			))}
-      </Tabs>
+		</Tabs>
 	);
 };
 export default TabBar;
