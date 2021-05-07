ad09d1f7993169eadb58ccf4e6aa9b3f03be4db1
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-05-07 11:16:58 +0200

    Made SortDropdown work with react-hook-form

--- src/components/app/SortDropdown.tsx
+++ src/components/app/SortDropdown.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
 import { css } from '@emotion/react';
+import { useTheme } from '@material-ui/core';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-import { useTheme } from '@material-ui/core';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import React from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
-interface SortDropdownProps {
-	children: React.ReactNode;
+export interface SortOption {
+	key: string;
+	title: string;
 }
+export type SortDropdownProps = Omit<UseFormRegisterReturn, 'ref'> & {
+	options: SortOption[];
+	inputRef: React.Ref<any>;
+};
 
-const SortDropdown: React.FC<SortDropdownProps> = ({ children }) => {
+const SortDropdown: React.FC<SortDropdownProps> = ({ options, name, onBlur, onChange, inputRef }) => {
 	const theme = useTheme();
 
 	return (
@@ -20,7 +27,11 @@ const SortDropdown: React.FC<SortDropdownProps> = ({ children }) => {
 			`}
 		>
 			<FormLabel>Sortieren nach: </FormLabel>
-			{children}
+			<NativeSelect name={name} onChange={onChange} onBlur={onBlur} inputRef={inputRef}>
+				{options.map((option) => (
+					<option value={option.key}>{option.key}</option>
+				))}
+			</NativeSelect>
 		</FormControl>
 	);
 };
