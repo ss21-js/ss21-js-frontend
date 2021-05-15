e47b8d54ecc15b09def2d97f20bd34eb1d02c488
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-05-15 22:27:51 +0200

    Cleaned up JobSearchBar component

--- src/components/search/JobSearchBar.tsx
+++ src/components/search/JobSearchBar.tsx
@@ -1,28 +1,65 @@
 import { css } from '@emotion/react';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { faDollarSign, faMapMarkerAlt, faSearch, faSuitcase } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { makeStyles, Theme } from '@material-ui/core';
+import { Theme, useTheme } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Autocomplete from '@material-ui/core/Autocomplete';
+import Divider from '@material-ui/core/Divider';
+import InputBase from '@material-ui/core/InputBase';
 import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import JobSearchBarTab from 'src/components/search/JobSearchBarTab';
+import React from 'react';
 import StyledButton from 'src/components/StyledButton';
 
-export interface SearchAppBarProps {
+interface SearchBarTabProps {
+	icon: IconProp;
+	title: string;
+}
+
+const JobSearchBarTab: React.FC<SearchBarTabProps> = ({ icon, title }) => {
+	const theme = useTheme();
+	const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+
+	return (
+		<>
+			<Divider
+				orientation="vertical"
+				flexItem
+				css={css`
+					margin-right: 20px;
+				`}
+			/>
+			<FontAwesomeIcon
+				icon={icon}
+				color="#0B77BF"
+				css={css`
+					margin-left: ${isMobile ? '10px' : '0'};
+					margin-top: ${isMobile ? '10px' : '0'};
+				`}
+			/>
+			<InputBase
+				placeholder={title}
+				multiline={false}
+				css={css`
+					color: ${theme.palette.secondary.contrastText};
+					padding-left: 0.5rem;
+					padding-right: 0.5rem;
+					text-overflow: ellipsis;
+					max-width: 250px;
+					font-weight: 500;
+				`}
+			/>
+		</>
+	);
+};
+
+export interface JobSearchBarProps {
 	onClick: () => void;
 }
 
-const useStyles = makeStyles(() => ({
-	root: {
-		paddingRight: 0,
-	},
-}));
-
-const SearchAppBar: React.FC<SearchAppBarProps> = ({ onClick }) => {
-	const classes = useStyles();
-
+const JobSearchBar: React.FC<JobSearchBarProps> = ({ onClick }) => {
 	const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
 
 	return (
@@ -37,7 +74,6 @@ const SearchAppBar: React.FC<SearchAppBarProps> = ({ onClick }) => {
 				`}
 			>
 				<Toolbar
-					className={classes.root}
 					css={css`
 						display: ${isMobile ? 'contents' : 'flex'};
 					`}
@@ -73,7 +109,7 @@ const SearchAppBar: React.FC<SearchAppBarProps> = ({ onClick }) => {
 					/>
 					<JobSearchBarTab icon={faMapMarkerAlt} title={'Stadt'} />
 					<JobSearchBarTab icon={faSuitcase} title={'Job Type'} />
-					<JobSearchBarTab icon={faDollarSign} title={'Buget'} />
+					<JobSearchBarTab icon={faDollarSign} title={'Budget'} />
 					<StyledButton
 						onClick={onClick}
 						css={css`
@@ -87,4 +123,5 @@ const SearchAppBar: React.FC<SearchAppBarProps> = ({ onClick }) => {
 		</div>
 	);
 };
-export default SearchAppBar;
+
+export default JobSearchBar;
