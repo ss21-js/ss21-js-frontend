f2a07261427276c3109ca7f5a57473e38c1cee51
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-05-19 17:33:24 +0200

    Fixed formatting

--- src/utils/formUtils.ts
+++ src/utils/formUtils.ts
@@ -6,23 +6,22 @@ type UseFormRegisterMaterialReturn = Omit<UseFormRegisterReturn, 'ref'> & {
 	helperText?: string;
 };
 
-export const useMaterialRegister = <T>(control: Control<T>) => (
-	name: Path<T>,
-	includeHelperText = true
-): UseFormRegisterMaterialReturn => {
-	const controller = useController({ name: name, control: control });
+export const useMaterialRegister =
+	<T>(control: Control<T>) =>
+	(name: Path<T>, includeHelperText = true): UseFormRegisterMaterialReturn => {
+		const controller = useController({ name: name, control: control });
 
-	let re: UseFormRegisterMaterialReturn = {
-		inputRef: controller.field.ref,
-		name: controller.field.name,
-		onBlur: async () => controller.field.onBlur(),
-		onChange: async (e) => controller.field.onChange(e),
-		error: controller.fieldState.error !== undefined,
+		let re: UseFormRegisterMaterialReturn = {
+			inputRef: controller.field.ref,
+			name: controller.field.name,
+			onBlur: async () => controller.field.onBlur(),
+			onChange: async (e) => controller.field.onChange(e),
+			error: controller.fieldState.error !== undefined,
+		};
+
+		if (includeHelperText) {
+			re = { ...re, helperText: controller.fieldState.error?.message };
+		}
+
+		return re;
 	};
-
-	if (includeHelperText) {
-		re = { ...re, helperText: controller.fieldState.error?.message };
-	}
-
-	return re;
-};
