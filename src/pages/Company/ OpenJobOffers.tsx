d69440216b9b329aac57cc0378e5e5788d1d2bbd
Carina Wehr <carina.wehr@stud.fh-rosenheim.de>
2021-05-27 14:01:45 +0200

    JS-61-openjoboffers page added

---
+++ src/pages/Company/ OpenJobOffers.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+import { Divider, Typography } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import React from 'react';
+import JobCard from 'src/components/JobCard';
+
+const UiDivider = styled(Divider)`
+	margin-top: 10px;
+`;
+
+const UiTypography = styled(Typography)`
+	margin-top: 10px;
+	margin-bottom: 10px;
+`;
+
+const OpenJobOffers: React.FC = () => {
+	function createData(description: string) {
+		return { description };
+	}
+
+    //Daten aus dem BE vom Job benutzen
+	const rows = [createData('UX Designer'), createData('React FE Developer' ), createData('Fullstack Developer')];
+
+	return (
+		<>
+			<JobCard
+				title="UI/UX Designer"
+				description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et"
+				image="https://preview.redd.it/iraq6sc6o3qz.jpg?auto=webp&s=dc0dfa00121359da7129d4efd9fbfc64635eac20"
+				tags={['Full-Time', '1 year Experience']}
+			/>
+			<UiDivider />
+			<UiTypography variant="h6">Vergangene Jobs: </UiTypography>
+			<TableContainer component={Paper}>
+				<Table aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Job Name:</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{rows.map((row) => (
+							<TableRow key={row.description}>
+								<TableCell component="th" scope="row">
+									{row.description}
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</>
+	);
+};
+
+export default OpenJobOffers;
