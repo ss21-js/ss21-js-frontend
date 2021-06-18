b02ae2457ba33ada0458b87bf03fb5b4a995b2dc
Veronika Starkulla <38860741+Veronika-Starkulla@users.noreply.github.com>
2021-06-18 15:54:44 +0200

    JS-53-Studentenprofil Header, AboutMe, Github Sektion hinzugefuegt

--- src/pages/Student/StudentenProfil.tsx
+++ src/pages/Student/StudentenProfil.tsx
@@ -1,39 +1,44 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import ProfileGithub from './ProfileGithub';
-import Center from '../../components/layout/Center';
 import { Student } from '../../model/profile/student';
+import GithubSection from './Sections/GithubSection';
+import Box from '@material-ui/core/Box';
+import AboutMeSection from './Sections/AboutMeSection';
+import ProfileHeaderSection from './Sections/ProfileHeaderSection';
+import Grid from '@material-ui/core/Grid';
 import { css } from '@emotion/react';
 
 export interface StudentenProfilProps {
-	student: Student;
+	student: Student
 }
-const StudentenProfil: React.FC<StudentenProfilProps> = ({ student }) => {
+
+const StudentenProfil: React.FC<StudentenProfilProps> = ({ student}) => {
+
 	return (
-		<Center>
-			<>
-				<Typography
-					css={css`
-						display: none;
-					`}
-				>
-					{student.githubusername}
-				</Typography>
-			</>
-			<>
-				<ProfileGithub
-					username={student.githubusername}
-					languages={[
-						{ name: 'Python', count: 12 },
-						{ name: 'JavaScript', count: 19 },
-						{ name: 'Flutter', count: 3 },
-						{ name: 'TypeScript', count: 5 },
-						{ name: 'Dart', count: 2 },
-						{ name: 'Java', count: 3 },
-					]}
-				/>
-			</>
-		</Center>
+		<Box marginTop={4}>
+			<ProfileHeaderSection/>
+			<Grid container css={css`
+				margin-left: auto;
+				margin-right: auto;
+				max-width: 1280px;
+				padding-left: 24px;
+				padding-right: 24px;`}>
+				<Grid item lg={8} md={9} sm={8} xs={12}>
+					<AboutMeSection student={student}/>
+				</Grid>
+				<Grid item lg={4} md={3} sm={4} xs={12}>
+					<GithubSection 	username={student.githubusername}
+									  languages={[
+										  { name: 'Python', count: 12 },
+										  { name: 'JavaScript', count: 19 },
+										  { name: 'Flutter', count: 3 },
+										  { name: 'TypeScript', count: 5 },
+										  { name: 'Dart', count: 2 },
+										  { name: 'Java', count: 3 },
+									  ]}
+					/>
+				</Grid>
+			</Grid>
+		</Box>
 	);
 };
 
