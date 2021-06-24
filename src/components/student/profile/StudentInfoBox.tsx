d1646a54503fea07923f7cea764d4afd13bbcead
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-06-24 09:26:37 +0200

    Added StudentInfoBox for profile

---
+++ src/components/student/profile/StudentInfoBox.tsx
@@ -0,0 +1,42 @@
+import InfoContainerGroup from 'components/app/InfoContainerGroup';
+import RoundedBox from 'components/RoundedBox';
+import { Student } from 'js-api-client';
+import React from 'react';
+import { WorkArea, WorkBasis } from 'store/jobs';
+
+export interface StudentInfoBoxProps {
+	student: Student;
+}
+
+const StudentInfoBox: React.FC<StudentInfoBoxProps> = ({ student }) => {
+	return (
+		<RoundedBox>
+			<InfoContainerGroup
+				items={[
+					{
+						title: 'Arbeitsbereich',
+						content:
+							student.workArea === WorkArea.NONE
+								? 'Keine Präferenz'
+								: `${student.workArea.substring(0, 1).toUpperCase()}${student.workArea.substring(1)}`,
+					},
+					{
+						title: 'Arbeitserfahrung',
+						content: student.yearsOfExperience + ' Jahr(e)',
+					},
+					{
+						title: 'Arbeitszeit',
+						content:
+							student.workBasis === WorkBasis.NONE
+								? 'Keine Präferenz'
+								: student.workBasis === WorkBasis.FULL_TIME
+								? 'Vollzeit'
+								: 'Teilzeit',
+					},
+				]}
+			/>
+		</RoundedBox>
+	);
+};
+
+export default StudentInfoBox;
