7266b26800651b09c98d00d9ede107bf31420d73
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-06-24 09:26:21 +0200

    Added complete student form for edit dialog

--- src/components/student/StudentForm.tsx
+++ src/components/student/StudentForm.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { Control } from 'react-hook-form';
 import StudentFormAddress from './StudentFormAddress';
 import StudentFormGeneral from './StudentFormGeneral';
+import StudentFormJob from './StudentFormJob';
+import StudentFormUniversity from './StudentFormUniversity';
 
 export interface StudentFormProps {
 	control: Control<Student>;
@@ -13,11 +15,17 @@ export interface StudentFormProps {
 const StudentForm: React.FC<StudentFormProps> = ({ control, disabled }) => {
 	return (
 		<Grid container spacing={4}>
-			<Grid item md={5} xs={12}>
+			<Grid item md={6} xs={12}>
 				<StudentFormGeneral control={control} disabled={disabled} />
 			</Grid>
-			<Grid item md={7} xs={12}>
+			<Grid item md={6} xs={12}>
 				<StudentFormAddress control={control} disabled={disabled} />
+			</Grid>
+			<Grid item md={6} xs={12}>
+				<StudentFormUniversity control={control} disabled={disabled} />
+			</Grid>
+			<Grid item md={6} xs={12}>
+				<StudentFormJob control={control} disabled={disabled} />
 			</Grid>
 		</Grid>
 	);
