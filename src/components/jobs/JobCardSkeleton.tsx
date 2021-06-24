d2bffdb2296aec94618798292107bb2c96565642
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-06-24 17:11:52 +0200

    Changed JobCardSkeleton styling

--- src/components/jobs/JobCardSkeleton.tsx
+++ src/components/jobs/JobCardSkeleton.tsx
@@ -7,7 +7,7 @@ const JobCardSkeleton: React.FC = () => {
 	return (
 		<RoundedBox padding={3}>
 			<RoundedBox width="72px" height="72px">
-				<Skeleton animation="wave" variant="circular" width={72} height={72} />
+				<Skeleton animation="wave" variant="rectangular" width={72} height={72} />
 			</RoundedBox>
 			<Box marginTop={1} marginBottom={0.5}>
 				<Skeleton animation="wave" variant="text" width="40%" />
